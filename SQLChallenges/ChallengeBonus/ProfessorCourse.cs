2a99a804e979a3a7757e500c5b26794f75355454
Jeffrey Hackett <Jeffth92@gmail.com>
2021-08-03 20:57:21 -0400

    fixed challenge some

--- SQLChallenges/ChallengeBonus/ProfessorCourse.cs
+++ SQLChallenges/ChallengeBonus/ProfessorCourse.cs
@@ -13,8 +13,8 @@ namespace SQLChallenges.ChallengeBonus
         public int ProfessorId { get; set; }
         public virtual Professor Professor { get; set; }
 
-        [ForeignKey(nameof(Student))]
-        public int StudentId { get; set; }
-        public virtual Student Student { get; set; }
+        [ForeignKey(nameof(Course))]
+        public int CourseId { get; set; }
+        public virtual Course Course { get; set; }
     }
 }
